f76576cccb33610f79d2fc22f18531ee19e1b46f
ejsawin <158220995+ejsawin@users.noreply.github.com>
2025-10-16 16:06:55 -0400

    Delete omega_cen directory

--- omega_cen/vel_calc.jl
+++
@@ -1,9 +0,0 @@
-# --- calculate radial, tangential velocity for (r,E,L)
-
-function v_r(rr,EE,LL) # Eq 1.6
-    return sqrt(abs(2*(EE-psi_calc(rr))-(LL^2)/(rr^2)))
-end
-
-function v_t(rr,EE,LL) # Eq 1.5
-    return LL/rr
-end
